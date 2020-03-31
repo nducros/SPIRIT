ab18e4f2112c8877c735235f45236bcf0a081ac9
ducros <nicolas.ducros@insa-lyon.fr>
2020-03-31 19:48:37 +0200

    Flip order of arguments call

--- function/datstat.m
+++ function/datstat.m
@@ -1,56 +1,50 @@
-function [C, mu] = datstat(Y, dataFolder, dataFilename, plotTag)
-% DATSTAT Computes or loads data statistics
+function [C, mu] = datstat(Y, dataSuffix, dataFolder,  plotTag)
+% DATSTAT Computes (and saves) database statistics
 %   C = DATSTAT(Y) returns the covariance matrix C corresponging to the
-%   image database Y.  The Nx-by-Ny-by-K database consists of K images each
-%   os size Nx-by-Ny. The covariance C is an N-by-M matrix, with N = Nx*Ny.
-%   The covariance matrix is either computed/saved or loaded from the
-%   disk.
-% 
-%   C = DATSTAT(Y, DATAFOLDER, DATAFILENAME) saves/loads the
-%   covariance from the file DATAFILENAME in the folder DATAFOLDER. The
-%   default folder is the current folder; the default filename is
-%   'mydata.mat'
-% 
-%   C = DATSTAT(..., PLOTTAG) plots the statistics when PLOTTAG is TRUE.
+%   image database Y.  The Nx-by-Ny-by-K database Y consists of K images 
+%   each of size Nx-by-Ny. The covariance C is an N-by-M matrix, with 
+%   N = Nx*Ny.
 %
-%   [C, MU] = DATSTAT(...) also computes or loads the Nx-by-Ny mean image
-%   MU.
+%   [C, MU] = DATSTAT(Y) also computes the Nx-by-Ny mean image MU.
+%   
+%   [C, MU] = DATSTAT(Y, DATASUFFIX) also saves as MAT-files the covariance
+%   and mean on in the current folder on the disk. DATASUFFIX is used as a 
+%   suffix for the filename. C is saved as 'cov_DATASUFFIX.mat' and MU as 
+%   'mean_DATASUFFIX.mat'.
+%
+%   [C, MU] = DATSTAT(Y, DATASUFFIX, DATAFOLDER) saves the MAT-files in the
+%   folder DATAFOLDER.
+% 
+%   [...] = DATSTAT(..., PLOTTAG) plots the statistics when PLOTTAG is TRUE.
 %
 %   Example:
 %   Todo!!
 %
 %   See Also: DATCOMP
-
+%
 %   Author: N. Ducros
 %   Institution: Creatis laboratory, University of Lyon, France
+%   Contact: nicolas.ducros@creatis.insa-lyon.fr
 %   Date: April 2019
 %   Toolbox: SPIRiT 2.0 https://github.com/nducros/SPIRIT
 %   License: CC-BY-SA 4.0 https://creativecommons.org/licenses/by-sa/4.0/
 
 %% Default values for inputs
 if nargin<4, plotTag = false; end
-if nargin<3, dataFilename = 'mydata'; end
+if nargin<3, dataSuffix = 'mydata'; end
 if nargin<2, dataFolder = '.\'; end
 
-%% Compute or load covariance matrix
-fullfilename = fullfile(dataFolder,['cov_',dataFilename]);
-if ~isfile(fullfilename) 
-    C = cov(reshape(Y,size(Y,1)*size(Y,2),[])');
-    save(fullfilename,'C');
-else
-   load(fullfilename,'C');
-end
+%% Compute and save covariance matrix
+fullfilename = fullfile(dataFolder,['cov_',dataSuffix]);
+C = cov(reshape(Y,size(Y,1)*size(Y,2),[])');
+save(fullfilename,'C');
 
 %% Compute or load save mean
 if nargout>1
-    fullfilename = fullfile(dataFolder,['mean_',dataFilename]);
-    if ~isfile(fullfilename) 
-        mu = mean(Y,3);
-        mu = mu(:);
-        save(fullfilename,'mu');   
-    else
-       load(fullfilename,'mu');
-    end
+    fullfilename = fullfile(dataFolder,['mean_',dataSuffix]);
+    mu = mean(Y,3);
+    mu = mu(:);
+    save(fullfilename,'mu');   
 end
 
 %% Plot
@@ -58,15 +52,15 @@ if plotTag
     if nargout>1
         figure; 
         subplot(121);
-        imagesc(abs(C)); axis image; colorbar;
-        title(['Covariance matrix ',dataFilename]);
+        imagesc(log(abs(C))); axis image; colorbar;
+        title(['Covariance matrix (log of magnitude) - ',dataSuffix]);
         %
         subplot(122);
-        plot(mu);
-        title(['Mean vector ',dataFilename]);
+        semilogy(abs(mu));
+        title(['Mean vector - ',dataSuffix]);
     else
         figure;
-        imagesc(abs(C)); axis image; colorbar;
-        title(['Covariance matrix ',dataFilename]);
+        imagesc(log(abs(C))); axis image; colorbar;
+        title(['Covariance matrix - ',dataSuffix]);
     end
 end
